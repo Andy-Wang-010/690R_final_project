cce119c5c80c558d0a057ba5b8b21aadfaf40e1e
Zhiheng Wang <zhihengwang@umass.edu>
2024-04-10 16:51:05 -0400

    backtrace succeed

--- plot_joints.py
+++ plot_joints.py
@@ -5,11 +5,8 @@ import matplotlib.cm as cmx
 from mpl_toolkits.mplot3d import Axes3D
 
 # Load data
-path1 = './amass-master/support_data/github_data/amass_sample.npz'
-data = np.load(path1, allow_pickle=True)
-marker_data = data['marker_data']
-print(list(data.keys()))
-# It's redundant to load the data twice and print the keys twice, so these lines are removed.
+data = np.load('./amass-master/support_data/github_data/amass_sample.npz', allow_pickle=True)
+a = data['marker_data'][0]
 labels = data['marker_labels']
 
 def scatter3d(x, y, z, cs, colorsMap='jet', labels=None):
@@ -17,7 +14,7 @@ def scatter3d(x, y, z, cs, colorsMap='jet', labels=None):
     cNorm = mcolors.Normalize(vmin=min(cs), vmax=max(cs))
     scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
     
-    fig = plt.figure(figsize=(10, 7))  # Adjusted figure size for better visualization
+    fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     scatter = ax.scatter(x, y, z, c=scalarMap.to_rgba(cs))
     
@@ -25,12 +22,12 @@ def scatter3d(x, y, z, cs, colorsMap='jet', labels=None):
     if labels is not None:
         for i, txt in enumerate(labels):
             ax.text(x[i], y[i], z[i], txt, size=5)
-    
+
     # Create a colorbar
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])  # Adjust these values as needed to prevent overlap
-    fig.colorbar(scalarMap, cax=cbar_ax, aspect=5)  # Removed shrink parameter for clarity
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    fig.colorbar(scalarMap, cax=cbar_ax, shrink=0.5, aspect=5)
 
     plt.show()
 
-# Adjusting the color scale input to match the length of marker_data instead of data
-scatter3d(marker_data[:, 0], marker_data[:, 1], marker_data[:, 2], np.arange(marker_data.shape[0]), labels=labels)
+# Call the function with marker labels
+scatter3d(a[:,0], a[:,1], a[:,2], np.arange(len(a)), labels=labels)
