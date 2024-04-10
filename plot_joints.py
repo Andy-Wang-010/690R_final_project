7c9e66121f8326f7afef90f7090cfa9fef7b8545
Zhiheng Wang <zhihengwang@umass.edu>
2024-04-10 16:28:16 -0400

    trace back attempt 1

--- plot_joints.py
+++ plot_joints.py
@@ -3,22 +3,21 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import matplotlib.cm as cmx
 from mpl_toolkits.mplot3d import Axes3D
-import os
 
 # Load data
 path1 = './amass-master/support_data/github_data/amass_sample.npz'
-path2 = './amass-master/support_data/github_data/A1_Stand_poses.npz'
-data = np.load(path2 , allow_pickle=True)[0]
-print(list(np.load(path1, allow_pickle=True).keys()))
+data = np.load(path1, allow_pickle=True)
+marker_data = data['marker_data']
 print(list(data.keys()))
-print(data['poses'].shape)
+# It's redundant to load the data twice and print the keys twice, so these lines are removed.
+labels = data['marker_labels']
 
 def scatter3d(x, y, z, cs, colorsMap='jet', labels=None):
     cm = plt.get_cmap(colorsMap)
     cNorm = mcolors.Normalize(vmin=min(cs), vmax=max(cs))
     scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
     
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10, 7))  # Adjusted figure size for better visualization
     ax = fig.add_subplot(111, projection='3d')
     scatter = ax.scatter(x, y, z, c=scalarMap.to_rgba(cs))
     
@@ -26,12 +25,12 @@ def scatter3d(x, y, z, cs, colorsMap='jet', labels=None):
     if labels is not None:
         for i, txt in enumerate(labels):
             ax.text(x[i], y[i], z[i], txt, size=5)
-
+    
     # Create a colorbar
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    fig.colorbar(scalarMap, cax=cbar_ax, shrink=0.5, aspect=5)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])  # Adjust these values as needed to prevent overlap
+    fig.colorbar(scalarMap, cax=cbar_ax, aspect=5)  # Removed shrink parameter for clarity
 
     plt.show()
 
-# Call the function with marker labels
-scatter3d(data[:,0], data[:,1], data[:,2], np.arange(len(data)), labels=labels)
+# Adjusting the color scale input to match the length of marker_data instead of data
+scatter3d(marker_data[:, 0], marker_data[:, 1], marker_data[:, 2], np.arange(marker_data.shape[0]), labels=labels)
