e01dd45ecb382733b8f0599ede216682110222fe
Zhiheng Wang <zhihengwang@umass.edu>
2024-04-11 11:20:49 -0400

    get the equal length for the axis

--- plot_joints.py
+++ plot_joints.py
@@ -4,7 +4,6 @@ import matplotlib.colors as mcolors
 import matplotlib.cm as cmx
 from mpl_toolkits.mplot3d import Axes3D
 
-# Load data
 data = np.load('./amass-master/support_data/github_data/amass_sample.npz', allow_pickle=True)
 a = data['marker_data'][0]
 labels = data['marker_labels']
@@ -18,17 +17,21 @@ def scatter3d(x, y, z, cs, colorsMap='jet', labels=None):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     scatter = ax.scatter(x, y, z, c=scalarMap.to_rgba(cs))
+
+    max_range = np.array([x.max()-x.min(), y.max()-y.min(), z.max()-z.min()]).max() / 2.0
+    mid_x = (x.max()+x.min()) * 0.5
+    mid_y = (y.max()+y.min()) * 0.5
+    mid_z = (z.max()+z.min()) * 0.5
+    ax.set_xlim(mid_x - max_range, mid_x + max_range)
+    ax.set_ylim(mid_y - max_range, mid_y + max_range)
+    ax.set_zlim(mid_z - max_range, mid_z + max_range)
     
-    # Annotate each point in 3D space with its label
     if labels is not None:
         for i, txt in enumerate(labels):
             ax.text(x[i], y[i], z[i], txt, size=5)
 
-    # Create a colorbar
     cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
     fig.colorbar(scalarMap, cax=cbar_ax, shrink=0.5, aspect=5)
-
     plt.show()
 
-# Call the function with marker labels
 scatter3d(a[:,0], a[:,1], a[:,2], np.arange(len(a)), labels=labels)
