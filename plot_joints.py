0833132e00a394bf212532504e7d888f7b1088a3
Zhiheng Wang <zhihengwang@umass.edu>
2024-04-14 08:41:58 -0400

    skeleton_plot

--- plot_joints.py
+++ plot_joints.py
@@ -5,9 +5,27 @@ import matplotlib.cm as cmx
 from mpl_toolkits.mplot3d import Axes3D
 
 data = np.load('./amass-master/support_data/github_data/amass_sample.npz', allow_pickle=True)
-a = data['marker_data'][0]
+marker_data = data['marker_data'][0]
 labels = data['marker_labels']
 print(labels)
+
+def skeleton_plot(markerData):
+    # Unpack the list of marker data into x, y, z coordinates
+    markerX, markerY, markerZ = zip(*markerData)
+    
+    # Create a new figure for 3D plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    # Plot lines between each adjacent vertex using a 3D line plot
+    ax.plot(markerX, markerY, markerZ, marker='o', linestyle='-', color='blue')  # 'o' creates a circle at each vertex
+    
+    # Set labels for the axes
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    
+    plt.show()
 
 def scatter3d(x, y, z, cs, colorsMap='jet', labels=None):
     cm = plt.get_cmap(colorsMap)
@@ -17,7 +35,7 @@ def scatter3d(x, y, z, cs, colorsMap='jet', labels=None):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     scatter = ax.scatter(x, y, z, c=scalarMap.to_rgba(cs))
-
+    
     max_range = np.array([x.max()-x.min(), y.max()-y.min(), z.max()-z.min()]).max() / 2.0
     mid_x = (x.max()+x.min()) * 0.5
     mid_y = (y.max()+y.min()) * 0.5
@@ -29,9 +47,12 @@ def scatter3d(x, y, z, cs, colorsMap='jet', labels=None):
     if labels is not None:
         for i, txt in enumerate(labels):
             ax.text(x[i], y[i], z[i], txt, size=5)
-
+    
     cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
     fig.colorbar(scalarMap, cax=cbar_ax, shrink=0.5, aspect=5)
+    
     plt.show()
 
-scatter3d(a[:,0], a[:,1], a[:,2], np.arange(len(a)), labels=labels)
+scatter3d(marker_data[:,0], marker_data[:,1], marker_data[:,2], np.arange(len(marker_data)), labels=labels)
+
+skeleton_plot(marker_data)
