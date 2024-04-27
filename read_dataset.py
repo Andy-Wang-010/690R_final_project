2048ff173e257bd6840c547283befbc59eccc524
rwbakerUMASS <rwbaker@umass.edu>
2024-04-26 20:35:59 -0400

    add npz compatibility

--- read_dataset.py
+++ read_dataset.py
@@ -34,3 +34,21 @@ for tar_filename in tar_files:
                         print(f"Error loading {member.name} from {tar_filename}: {e}")
                     finally:
                         file.close()
+
+            if member.isfile() and member.name.endswith('.npz'):
+                # Extract the member as a file-like object
+                print('I am still reading!!!')
+                file = tar.extractfile(member)
+                if file:
+                    try:
+                        # Load the .npy file directly from the tar archive
+                        data = np.load(file, allow_pickle=True)
+                        # Process the data here
+                        for key in list(data.keys()):
+                            print(key)
+                            # If dt is a pandas DataFrame, use dt.head()
+                            # Ensure the appropriate method calls based on your data structure
+                    except Exception as e:
+                        print(f"Error loading {member.name} from {tar_filename}: {e}")
+                    finally:
+                        file.close()
