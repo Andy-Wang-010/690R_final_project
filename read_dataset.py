5e85e3c397a6cce20b06c117956b5bded4aac5ff
rwbakerUMASS <rwbaker@umass.edu>
2024-04-26 20:51:39 -0400

    Informative feedback

--- read_dataset.py
+++ read_dataset.py
@@ -7,19 +7,18 @@ directory_path = './data'
 
 # List all .tar.bz2 files in the directory
 tar_files = [f for f in os.listdir(directory_path) if f.endswith('.tar.bz2')]
-
 # Process each .tar.bz2 file
 for tar_filename in tar_files:
-    print('no rush, I am reading!!!')
+    print(f'Reading: {tar_filename}')
     tar_path = os.path.join(directory_path, tar_filename)
     with tarfile.open(tar_path, 'r:bz2') as tar:
         # Process each member in the .tar.bz2 file
         for member in tar.getmembers():
-            print('I am in second for loop')
+            print(f'Reading: {member.name}')
             # Check if the member is a file and ends with .npy
             if member.isfile() and member.name.endswith('.npy'):
                 # Extract the member as a file-like object
-                print('I am still reading!!!')
+                print('Extracting...')
                 file = tar.extractfile(member)
                 if file:
                     try:
@@ -37,15 +36,16 @@ for tar_filename in tar_files:
 
             if member.isfile() and member.name.endswith('.npz'):
                 # Extract the member as a file-like object
-                print('I am still reading!!!')
+                print('Extracting...')
                 file = tar.extractfile(member)
                 if file:
                     try:
                         # Load the .npy file directly from the tar archive
                         data = np.load(file, allow_pickle=True)
                         # Process the data here
+                        print([key for key in list(data.keys())])
                         for key in list(data.keys()):
-                            print(key)
+                            pass
                             # If dt is a pandas DataFrame, use dt.head()
                             # Ensure the appropriate method calls based on your data structure
                     except Exception as e:
